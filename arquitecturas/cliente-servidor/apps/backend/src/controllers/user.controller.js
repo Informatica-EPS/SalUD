06ac241106eaf08bcad30b57c5c3e4305cbed0bf
Andres Ariza <and.694@gmail.com>
2026-02-28 17:11:00 -0500

    Initializing Node project

---
+++ arquitecturas/cliente-servidor/apps/backend/src/controllers/user.controller.js
@@ -0,0 +1,25 @@
+
+const userService = require('../services/user.service');
+
+const createUser = async (req, res, next) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await userService.getUsers();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createUser,
+  getUsers
+};
---
+++ arquitecturas/cliente-servidor/apps/backend/src/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+// const sequelize = require("./config/database");
+// const routes = require("./routes");
+const errorHandler = require("./middlewares/error.middleware");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Welcome to the SalUD backend API!", status: "ok" });
+});
+
+// app.use("/api", routes);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+// sequelize.sync().then(() => {
+// });
