e15e74c28b417d57b78fcf0ed7b3ba00436980d7
afarizal1099 <afarizal@udistrital.edu.co>
2026-03-02 12:47:56 -0500

    Revert "Merge Develop into Main"

--- arquitecturas/cliente-servidor/apps/backend/src/routes/patient.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const patientController = require("../controllers/patient.controller");
-
-router.post("/", patientController.createPatient);
-router.get("/", patientController.getPatients);
-
-module.exports = router;
