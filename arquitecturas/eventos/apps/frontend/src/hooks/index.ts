86ce1f2e4131d0a5f7db3e69585cd716baef2038
iamstivgo <iamstivgo@gmail.com>
2026-04-10 14:28:56 -0500

    copiar proyecto front en eventos

---
+++ arquitecturas/eventos/apps/frontend/src/hooks/index.ts
@@ -0,0 +1,5 @@
+// Hooks personalizados para la aplicación
+export { usePatientAppointments } from './usePatientAppointments';
+export { useDoctorTimeSlots } from './useDoctorTimeSlots';
+export { useDoctors } from './useDoctors';
+export { useCreateAppointment } from './useCreateAppointment';
