5ae8f08f0f9f0c2f2b7f5a451568f5e1d65d8e8a
Niny Parra <njparrag@udistrital.edu.co>
2026-04-16 21:37:14 -0500

    Update dev.bat

--- arquitecturas/eventos/dev.bat
+++ arquitecturas/eventos/dev.bat
@@ -1,2 +1,2 @@
 @echo off
-docker-compose --env-file infra/dev.env -f infra/docker-compose.yaml -f infra/docker-compose.dev.yaml up --build
+docker-compose --env-file infra/dev.env -f infra/docker-compose.dev.yaml up --build
