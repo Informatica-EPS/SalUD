99c61f2a6299a1c9ee0c71060d4399f88c4b1283
DannaPaolaCP24 <dpcaperap@udistrital.edu.co>
2026-04-17 21:43:25 -0500

    Fix: Merge con develop

--- arquitecturas/eventos/apps/frontend/src/hooks/index.ts
+++ arquitecturas/eventos/apps/frontend/src/hooks/index.ts
@@ -4,8 +4,4 @@ export { useDoctorTimeSlots } from './useDoctorTimeSlots';
 export { useDoctors } from './useDoctors';
 export { useCreateAppointment } from './useCreateAppointment';
 export { useOrders } from './useOrders';
-<<<<<<< HEAD
 export { useSpecialties } from './useSpecialties';
-=======
-export { useSpecialties } from './useSpecialties';
->>>>>>> f0efdc241dc7e3839f62ddefbd4b096c8395392d
