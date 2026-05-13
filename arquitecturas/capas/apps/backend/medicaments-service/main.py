410b92ec87314fc363108c3e140b602f63b39873
jsdiazr <jsediazr@udistrital.edu.co>
2026-05-13 18:43:41 -0500

    feat: cors

--- arquitecturas/capas/apps/backend/medicaments-service/main.py
+++ arquitecturas/capas/apps/backend/medicaments-service/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Medicaments Service", version="1.0")
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:3000", "http://localhost:8081"],
+    allow_origins=["http://localhost:8080", "http://localhost:3000", "http://localhost:8081","https://blue-rock-0a2badb0f.7.azurestaticapps.net/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
